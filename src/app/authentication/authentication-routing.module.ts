7000f1ea77434ca29583be5bcd0cdf3bacba7357
totumix <totucr95@gmail.com>
2023-09-06 17:55:13 -0500

    se agregó el formulario de registro

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'login',
     component: LoginComponent,
     data: {
       title: 'Login'
+    }
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    data: {
+      title: 'Register'
     }
   },
   {
--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { LoginComponent } from './login/login.component';
-
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
@@ -12,6 +10,9 @@ import { ButtonModule } from '../shared/components/button/button.module';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { MessagesComponent } from '../shared/components/messages/messages.component';
 import { BusinessFormComponent } from '../shared/components/business-form/business-form.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeAuthenticationComponent } from './home-authentication/home-authentication.component';
+import { RouterModule } from '@angular/router';
 
 const antdModule= [
   NzFormModule,
@@ -21,7 +22,9 @@ const antdModule= [
 ]
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    HomeAuthenticationComponent,
   ],
   imports: [
     CommonModule,
