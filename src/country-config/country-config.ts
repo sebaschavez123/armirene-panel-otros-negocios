d5cba07c8c93ee2a1c2e12e3a03522a342045c77
totumix <totucr95@gmail.com>
2023-09-25 17:43:29 -0500

    implementacion del phoneSize para ambos paises e implementación del card de primer inicio de sesión

--- src/country-config/country-config.ts
+++ src/country-config/country-config.ts
@@ -5,21 +5,7 @@ const configuration: any = {
   countryName: environment.countryName,
   isColombia: environment.isColombia,
   isVenezuela: environment.isVenezuela,
-  // backend3: environment.gateway30,
-  // antifraud: environment.gateway30Antifraud,
-  // dashboard: environment.gateway30Dashboard,
-  // reports: environment.gateway30Reports,
-  // firebase: environment.gatewayFirebase,
-  // sas: environment.gateway30SAS,
-  // sb: environment.gatewaySB,
-  // firebaseConfig: environment.firebaseConfig,
-  // indicator: environment.indicator,
-  // currency: environment.currency,
-  // cctld: environment.cctld,
-  // phoneCode: environment.isColombia ? '57':'58',
-  // coordinates: environment.coordinates,
-  // payments: environment.payments,
-  // zendesk: environment.zendesk,
+  phoneSize: environment.phoneSize
 };
 
-export {configuration as countryConfig};
+export { configuration as countryConfig };
