a65a341aaa2cef2905307ea032fb9ad2f1296a55
totumix <totucr95@gmail.com>
2023-11-23 16:12:41 -0500

    hotfix al crear una orden

--- src/app/core/baseForm/base-form-order.service.ts
+++ src/app/core/baseForm/base-form-order.service.ts
@@ -26,7 +26,7 @@ export class BaseFormOrderService {
 
     setValidators(order: any, form: FormGroup) {
         Object.keys(order).forEach(key => {
-            if (key != 'instructions' && key != 'orderId' && key != 'dni' && key != 'addressDetail' && key != 'indications') {
+            if (key != 'instructions' && key != 'orderId' && key != 'dni' && key != 'addressDetail' && key != 'indications' && key != 'createDate' && key != 'state') {
                 form.get(key)?.setValidators(Validators.required);
             }
             if (key == 'email') {
